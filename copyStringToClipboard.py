a4bbe2a6fa44e6dc6a3554d0a459902df0dfbe5f
sanchit_raina <rainasanchit01@gmail.com>
2019-01-31 11:15:20 +0530

    Copying a string to clipboard and modify that string using python

---
+++ copyStringToClipboard.py
@@ -0,0 +1,18 @@
+PASSWORDS = {
+	'email': 'computerengineer',
+	'blog': '#iamblogger#',
+	'luggage': '12345'
+}
+
+import sys, pyperclip
+if len(sys.argv) < 2:
+	print('Usage: python pw.py [account] - copy account password')
+	sys.exit()
+
+account = sys.argv[1]
+
+if account in PASSWORDS:
+	pyperclip.copy(PASSWORDS[account])
+	print('Password for', account, 'copied to the clipboard.')
+else:
+	print('There is no account named', account)
